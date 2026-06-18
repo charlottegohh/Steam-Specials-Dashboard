916339c67533466ac0f88a66c5ce581a946e5440
Charlotte <charlotteanne27@gmail.com>
2026-06-18 17:56:23 +0800

    removed binary location

--- steamScraper.py
+++ steamScraper.py
@@ -11,7 +11,6 @@ def scrape_steam():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.binary_location = '/usr/bin/chromium'
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
